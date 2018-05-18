e9edba68603040b9a517acca95486f2ba879cb88
Niall <niallmcclelland166@hotmail.com>
2018-05-18 18:41:32 +0100

    Adding connection string for CustomersContext in web config, there are now two connection strings in web Config.

--- shop2Tests2/TestCustomerController.cs
+++ shop2Tests2/TestCustomerController.cs
@@ -11,19 +11,60 @@ namespace shop2Tests2
         [TestMethod()]
 
         public void CreateCustomerTest()
-        { 
-        TestCustomersContext tbc = new TestCustomersContext();
-        CustomerController controller = new CustomerController(tbc);
-        Customer customerToCreate = new Customer() { CustomerID = 13, CName = "Bob", CAddress = "Tallaght", Phone = "four" };
-        // Act
+        {
+            TestCustomersContext tbc = new TestCustomersContext();
+            CustomerController controller = new CustomerController(tbc);
+            Customer customerToCreate = new Customer() { CustomerID = 13, CName = "Bob", CAddress = "Tallaght", Phone = "four" };
+            // Act
 
-        var result = controller.Create(customerToCreate) as RedirectToRouteResult;
+            var result = controller.Create(customerToCreate) as RedirectToRouteResult;
 
-        // Assert
+            // Assert
 
-        Assert.AreEqual("Index", result.RouteValues["action"]);
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+
+        }
 
 
+       [TestMethod()]
+
+        public void IndexTestNameLike()
+
+        {
+
+        //    //Arrange
+
+        //    TestCustomersContext tbc = new TestCustomersContext();
+
+        //    CustomerController controller = new CustomerController(tbc);
+
+        //    Customer Carol = new MeetingRoom() { Name = "Chat Room", Size = 4, Location = "Ground Floor" };
+
+        //    MeetingRoom room2 = new MeetingRoom() { Name = "Board Room", Size = 8, Location = "First Floor" };
+
+        //    tbc.MeetingRooms.Add(room1);
+
+        //    tbc.MeetingRooms.Add(room2);
+
+        //    // Act
+
+        //    ViewResult result = controller.Index("", "hat") as ViewResult;
+
+        //    // Assert
+
+        //    var rooms = (List<MeetingRoom>)result.ViewData.Model;
+
+        //    foreach (var r in rooms)
+
+        //    {
+
+        //        StringAssert.Contains(r.Name, "hat");
+
+        //    }
+
+        //    Assert.AreEqual(1, rooms.Count);
+
+        //}
 
         //[TestMethod()]
         //public void IndexTest()
@@ -42,7 +83,7 @@ namespace shop2Tests2
 
         //    var result = controller.Create(cust);
         //    Assert.IsNotNull(result);
-        }
+    }
 
     }
 }
