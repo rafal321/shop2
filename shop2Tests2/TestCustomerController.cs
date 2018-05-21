dd6a4b8772b8cc9e3c0f93e97942d3070f64d303
AlinedaSilva <35935280+AlinedaSilva@users.noreply.github.com>
2018-05-21 20:26:13 +0100

    Final Super dupper final V1

--- shop2Tests2/TestCustomerController.cs
+++ shop2Tests2/TestCustomerController.cs
@@ -24,37 +24,18 @@ namespace shop2Tests2
             Assert.AreEqual("Index", result.RouteValues["action"]);
 
         }
-
         [TestMethod()]
-
-        public void IndexTestSpecficWord()
-
+        public void EditCustomerTest()
         {
+            TestCustomersContext C = new TestCustomersContext();
+            CustomerController controller = new CustomerController(C);
+            Customer customerToEdit = new Customer() { CustomerID = 13, CName = "Bob", CAddress = "Tallaght", Phone = "08744458" };
 
-            TestCustomersContext c = new TestCustomersContext();
+            var result = controller.Edit(customerToEdit) as RedirectToRouteResult;
 
-            CustomerController controller = new CustomerController(c);
-
-            Customer Customer1 = new Customer() { CName = "Bob", CAddress = "Dublin", };
-
-            c.Customers.Add(Customer1);
-
-            ViewResult result = CustomerController.Index("", "Dublin") as ViewResult;
-
-            var customers = (List<Customer>)result.ViewData.Model;
-
-            foreach (var r in customers)
-
-            {
-
-                StringAssert.Contains(r.CAddress, "Dublin");
-
-            }
-
-            Assert.AreEqual(1, customers.Count);
+            Assert.AreEqual("Index", result.RouteValues["action"]);
 
         }
-       
 
     }
 }
